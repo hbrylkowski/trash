0033c486be121afb6159c61e47d050b96a71fc6b
Hubert Bryłkowski <hubert@brylkowski.com>
2024-06-22 21:50:39 +0200

    prettier

--- src/app.tsx
+++ src/app.tsx
@@ -9,12 +9,31 @@ import green from "./assets/trashcans/green.png"
 import brown from "./assets/trashcans/brown.png"
 import recycle from "./assets/trashcans/recycle.png"
 
+const fuse = new Fuse(trashTargets, {
+    keys: ['name'],
+    includeScore: true,
+})
+
+function nameToImage(name: string) {
+    if (name.includes("zielony")) {
+        return green
+    }
+    if (name.includes("niebieski")) {
+        return blue
+    }
+    if (name.includes("żółty")) {
+        return yellow
+    }
+    if (name.includes("brązowy")) {
+        return brown
+    }
+    if (name.includes("czarny")) {
+        return black
+    }
+    return recycle
+}
 
 export function App() {
-    const fuse = new Fuse(trashTargets, {
-        keys: ['name'],
-        includeScore: true,
-    })
     const [searchResults, setSearchResults] = useState([])
 
     function searchTrash(phrase: string) {
@@ -22,62 +41,52 @@ export function App() {
         setSearchResults(results)
     }
 
-    function nameToImage(name: string) {
-        if (name.includes("zielony")) {
-            return green
-        }
-        if (name.includes("niebieski")) {
-            return blue
-        }
-        if (name.includes("żółty")) {
-            return yellow
-        }
-        if (name.includes("brązowy")) {
-            return brown
-        }
-        if (name.includes("czarny")) {
-            return black
-        }
-        return recycle
-    }
 
     return (
         <>
-            <h1>Śmiecioszukajka</h1>
-            <div class="card">
-                <input type="text"
-                       class="bg-gray-900 w-full p-3 m-2 border-2 border-gray-300 rounded bg-gray-100 text-lg focus:border-green-500 focus:bg-green-100 focus:text-gray-950 transition-colors duration-300 ease-in-out"
-                       onInput={(a) => searchTrash(a.target.value)}/>
-                <div>
-                    {searchResults.map(result => {
-                        const item = result.item
-                        return (
-                            <div key={item.id}
-                                 class="bg-gray-900 max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
-                                <div class="md:flex">
-                                    <div class="md:flex-shrink-0">
-                                        <img class="h-24 h-full object-cover md:w-36"
-                                             src={nameToImage(item.targets[0].name)}
-                                             alt={item.name}/>
-                                    </div>
-                                    <div class="p-8">
-                                        <div
-                                            class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{item.name}</div>
-                                        {item.targets.map(target => {
-                                            if (target.id != "IMG") {
-                                                return (
-                                                    <div key={target.id}
-                                                         class="p-2 m-2 border-2 border-gray-300 rounded bg-gray-800 text-lg">
-                                                        {target.name}
-                                                    </div>
-                                                )
-                                            }
-                                        })}
+            <div class="">
+                <div class="text-center mb-6">
+                    <h1 class="text-4xl sm:text-5xl md:text-6xl">Śmiecioszukajka</h1>
+                </div>
+                <div class="w-full">
+                    <input type="text"
+                           className="w-full p-3 bg-gray-900 border-2 border-gray-300 rounded bg-gray-100 text-lg focus:border-green-500 focus:bg-green-100 focus:text-gray-950 transition-colors duration-300 ease-in-out"
+                           onInput={(a) => searchTrash(a.target.value)}/>
+
+                </div>
+
+                <div class="card">
+                    <div>
+                        {searchResults.map(result => {
+                            const item = result.item
+                            return (
+                                <div key={item.id}
+                                     class="bg-gray-900 max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
+                                    <div class="md:flex">
+                                        <div class="hidden md:block md:flex-shrink-0 flex items-center">
+                                            <img class="max-w-0 md:max-w-36 p-4"
+                                                 src={nameToImage(item.targets[0].name)}
+                                                 alt={item.name}/>
+                                        </div>
+                                        <div class="p-2 w-full">
+                                            <div
+                                                class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{item.name}</div>
+                                            {item.targets.map(target => {
+                                                if (target.id != "IMG") {
+                                                    return (
+                                                        <div key={target.id}
+                                                             class="p-2 my-2 border-2 border-gray-300 rounded bg-gray-800 text-lg w-full">
+                                                            {target.name}
+                                                        </div>
+                                                    )
+                                                }
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </>
